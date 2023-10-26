c73423ab4550ec3ff4783cc7c154ce4e493e1769
JoaoGabrielVianna <joaogabrielvianna05@gmail.com>
2023-10-26 02:07:25 -0300

    iniciando parte das faculdades

---
+++ src/components/faculdadesBar/FaculdadesBar.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import './FaculdadesBar.css';
+import DadosFaculdade from '../../data/faculdades.json';
+import Follow from "../../assets/svgs/icons/follow/Follow";
+function FaculdadesBar({hide}){
+    const [follow, setFollow] = useState(0);
+
+    return(
+        <>
+            <ul id="main_FaculdadesBar" style={{height: hide ? '150px' : '0px', transition: '.5s ease-in-out'}}>
+                {DadosFaculdade.map((faculdade, i) => (
+                    <div key={faculdade.id}>
+                        <img src={faculdade.foto} alt="" />
+                        <h1>{faculdade.nome}</h1>
+                        <div className="follow_button" onClick={() => setFollow(faculdade.id)}><Follow follow={follow === faculdade.id ? follow : ''}/></div>
+                        
+                        
+                    </div>
+                ))}
+        </ul>
+        </>
+    )
+}
+
+export default FaculdadesBar;
--- src/routes/HomeScreen/Home.jsx
+++ src/routes/HomeScreen/Home.jsx
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useState} from "react";
 import './Home.css';
 import Search_icon from "../../assets/svgs/icons/search-icon/Search-icon";
 import Notification_icon from "../../assets/svgs/icons/notification-icon/Notification-icon";
+import Up_Down from "../../assets/svgs/icons/up_down/Up_Down";
+import FaculdadesBar from "../../components/faculdadesBar/FaculdadesBar";
+
+const Header_Home = () => {
+    const [hide, setHide] = useState(false);
+
+    return(
+        <>
+            <header id="header_Home" style={{height: hide ? 280 : ''}}>
+                    <div className="search_bar">
+                        <span>
+                            <Search_icon/>
+                            <input placeholder="Pesquisar"/>
+                        </span> 
+                            <div className="notification_div">
+                                <Notification_icon/>
+                            </div>
+                        
+                    </div>
+                    
+                    <div className="faculdade_bar">
+                        <h1>Faculdades</h1>
+                        <div className="icon" onClick={() => setHide(!hide)}>
+                            <Up_Down state={hide}/>
+                        </div>
+                    </div>
+                        <FaculdadesBar hide={hide}/>
+                    
+                </header>
+                
+        </>
+    )
+}
 
 function Home(){
     return(
         <>
-            <header id="header_Home">
-                    <div className="search_bar">
-                        <Search_icon/>
-                        <input placeholder="Pesquisar"/>
-                    </div>
-                    <div className="notification_div">
-                        <Notification_icon/>
-                    </div>
-                    
-                </header>
+            <Header_Home/>
                 
             <main id="main_Home">
                 
