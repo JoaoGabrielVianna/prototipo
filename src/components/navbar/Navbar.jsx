4d3532d731e659e0b479fca1c10117e36e0be9b7
JoaoGabrielVianna <joaogabrielvianna05@gmail.com>
2023-10-25 21:51:32 -0300

    NavBar concluida

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import './Navbar.css';
+import Divisor from "../../assets/svgs/icons/components/Divisor";
 import Faculdade_icon from "../../assets/svgs/icons/components/Faculdade-icon";
 import Destaques_icon from "../../assets/svgs/icons/components/Destaques-icon";
 import Home_icon from "../../assets/svgs/icons/components/Home-icon";
 import Eventos_icon from "../../assets/svgs/icons/components/Eventos-icon";
 import Profile_icon from "../../assets/svgs/icons/components/Profile-icon";
-import { Link } from "react-router-dom";
+
 
 function NavBar(){
     const [active, setActive] = useState(2);
     const icons =[
-        {name: 'faculdade', local: '/faculdade', iconD: <Faculdade_icon/>, iconA: <Faculdade_icon ativo={true}/>, posX: -0.5},
-        {name: 'destaques', local: '/destaques', iconD: <Destaques_icon/>, iconA: <Destaques_icon ativo={true}/>, posX: 18.6},
-        {name: 'home', local: '/', iconD: <Home_icon/>, iconA: <Home_icon ativo={true}/>, posX: 37.6},
-        {name: 'eventos', local: '/eventos', iconD: <Eventos_icon/>, iconA: <Eventos_icon ativo={true}/>, posX: 57.2},
-        {name: 'profile', local: '/profile', iconD: <Profile_icon/>, iconA: <Profile_icon ativo={true}/>, posX: 76.3},
+        {name: 'faculdade', local: '/faculdade', iconD: <Faculdade_icon/>, iconA: <Faculdade_icon ativo={true}/>, posX: -100.5},
+        {name: 'destaques', local: '/destaques', iconD: <Destaques_icon/>, iconA: <Destaques_icon ativo={true}/>, posX: -82},
+        {name: 'home', local: '/', iconD: <Home_icon/>, iconA: <Home_icon ativo={true}/>, posX: -62.5},
+        {name: 'eventos', local: '/eventos', iconD: <Eventos_icon/>, iconA: <Eventos_icon ativo={true}/>, posX: -43},
+        {name: 'profile', local: '/profile', iconD: <Profile_icon/>, iconA: <Profile_icon ativo={true}/>, posX: -23},
     ]
 
     return(
@@ -31,7 +33,8 @@ function NavBar(){
                         </li>
                     ))}
                 </div>
-                <span style={{left: `${icons[active].posX}%`, transition: '.5s ease'}} className="indicator"></span>
+                <span className="divisor" style={{left: `${icons[active].posX}%`, transition: '.5s ease'}}><Divisor/></span>
+                {/* <span style={{left: `${icons[active].posX}%`, transition: '.5s ease'}} className="indicator"></span> */}
             </main>
             </span>
         </>
